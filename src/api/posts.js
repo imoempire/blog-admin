510be985459044b105fe49ad8528b2be6002b00e
imoempire <mrimoempire@gmail.com>
2022-04-03 11:20:13 +0000

    connecting to backend

---
+++ src/api/posts.js
@@ -0,0 +1,53 @@
+import client from "./clients"
+
+ export const getPosts = async (pageNo, limit) => {
+      try {
+      const {data} = await client(`/post/posts?pageNo=${pageNo}&limit=${limit}`);
+      return data;
+      } catch (error) {
+         const {response} = error;
+         if (response?.data) {
+            return response.data;
+         }
+         return { error: error.message || error }; 
+      }
+ };
+
+ export const deletePosts = async (postId) => {
+   try {
+   const {data} = await client.delete(`/post/${postId}`);
+   return data
+   } catch (error) {
+      const {response} = error;
+      if(response?.data){
+         return response.data;
+      }
+      return {error: error.message || error }; 
+   }
+}
+
+export const searchPost = async (query) => {
+   try {
+   const {data} = await client(`/post/search?title=${query}`);
+   return data
+   } catch (error) {
+      const {response} = error;
+      if(response?.data){
+         return response.data;
+      }
+      return {error: error.message || error }; 
+   }
+}
+
+export const uploadImage = async (formData) => {
+   try {
+   const {data} = await client.post(`/post/uploadImage`, formData);
+   return data;
+   } catch (error) {
+      const {response} = error;
+      if(response?.data){
+         return response.data;
+      }
+      return {error: error.message || error }; 
+   }
+}
